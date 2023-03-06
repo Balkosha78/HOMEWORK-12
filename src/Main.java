9892732fc0e7cf94e0d7d1e3966b4012e92c376f
Алексей <balko-aleksey@yandex.ru>
2023-03-06 21:35:39 +0700

    HW 12  completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author = new Author("Лев", "Толстой");
+        Author author1 = new Author("Фёдор", "Достоевский");
+        Book warAndPeas = new Book("Война и мир", 1967, author);
+        Book crimeAndPunishment = new Book("Преступление и наказание", 1866, author1);
+        System.out.println(crimeAndPunishment.getAuthor() + crimeAndPunishment.getName() + crimeAndPunishment.getPublicationYear());
+        System.out.println(warAndPeas.getAuthor() + warAndPeas.getName() + warAndPeas.getPublicationYear());
     }
 }
